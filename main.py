f9a6655c21699e3a82c3d6e4c93bacbaa94ee2d6
Travis Sage <travis.j.sage@gmail.com>
2023-12-02 18:33:36 -0700

    Update main to use function dictionary

--- main.py
+++ main.py
@@ -2,30 +2,16 @@ from Day01 import day01
 from Day02 import day02
 
 if __name__ == '__main__':
-    day = 2
+    input_file = "./inputs/real-02.txt"
+    day = "02"
+    part2 = True
 
-    if day == 1:
-        part1_test = day01("./inputs/test-01.txt", False)
-        print(f"Part 1 Test: {part1_test}")
+    day_functions = {
+        "01": day01,
+        "02": day02
+    }
 
-        part1_real = day01("./inputs/real-01.txt", False)
-        print(f"Part 1 Real: {part1_real}")
+    answer = day_functions[day](input_file, part2)
+    print(f"Answer: {answer}")
 
-        part2_test = day01("./inputs/test-01-b.txt", True)
-        print(f"Part 2 Test: {part2_test}")
 
-        part2_real = day01("./inputs/real-01.txt", True)
-        print(f"Part 2 Real: {part2_real}")
-
-    elif day == 2:
-        part1_test = day02("./inputs/test-02.txt", False)
-        print(f"Part 1 Test: {part1_test}")
-
-        part1_real = day02("./inputs/real-02.txt", False)
-        print(f"Part 1 Real: {part1_real}")
-
-        part2_test = day02("./inputs/test-02.txt", True)
-        print(f"Part 2 Test: {part2_test}")
-
-        part2_real = day02("./inputs/real-02.txt", True)
-        print(f"Part 2 Real: {part2_real}")
